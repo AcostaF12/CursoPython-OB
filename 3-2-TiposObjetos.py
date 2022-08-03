9e33d66ee44414774216d1c40cac534b027fe3f0
AcostaF12 <107961278+AcostaF12@users.noreply.github.com>
2022-08-02 22:24:45 -0300

    Add files via upload

---
+++ 3-2-TiposObjetos.py
@@ -0,0 +1,16 @@
+nombre = 'Cosme'
+apellido = 'Fulanito'
+edad = 24
+email = 'cosmef120@gmail.com'
+telefono = '562-902-1694'
+casado = True
+conHijos = False
+listaDeAmigos = ['Dr. Hibbert', 'Moe', 'Barney']
+peliculasVistas = {
+    'clave1': 'Inglorius Bastards',
+    'clave2': 'Operacion Final',
+    'clave3': 'Rescatando al Soldado Ryan',
+    'clave4': 'La lista de Schindler'
+}
+
+
