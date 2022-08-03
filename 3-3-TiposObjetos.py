1052fde6be3f184944fb80fdf216ff6ff57ab1d3
AcostaF12 <107961278+AcostaF12@users.noreply.github.com>
2022-08-02 22:36:46 -0300

    Add files via upload

--- 3-3-TiposObjetos.py
+++ 3-3-TiposObjetos.py
@@ -1,5 +1,5 @@
-pesoUsuario = 67
-estaturaUsuario = 1.83
+pesoUsuario = input("¿Cual es tu peso en Kg?")
+estaturaUsuario = input("¿Cual es tu estatura?")
 
 masaCorporalUsuario = pesoUsuario / estaturaUsuario
 print(round(masaCorporalUsuario, 2))
