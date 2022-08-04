16b64adc7c500d668e80527a13819a387ada275c
AcostaF12 <107961278+AcostaF12@users.noreply.github.com>
2022-08-04 13:51:39 -0300

    Add files via upload

---
+++ 6-E1-ClasesYObjetos.py
@@ -0,0 +1,18 @@
+class Vehiculo():
+    def __init__(self, color, ruedas, puertas):
+        self.color = color
+        self.ruedas = ruedas
+        self.puertas = puertas
+    
+        
+class Coche(Vehiculo):
+    def __init__(self, color, ruedas, puertas, velocidad, cilindrada):
+        self.color = color
+        self.ruedas = ruedas
+        self.puertas = puertas
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+
+coche = Coche("Gris", 4, 3, "250km/h", "850cc")
+print(coche)
+
